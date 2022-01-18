df12f19f5ec0a0e265cd4293fbdccec9e6b7c161
Paulo Bordignon <p.h_@outlook.com>
2022-01-18 09:37:03 -0300

    fix(component): fixed bug in deploy with alert component

--- src/components/atoms/Alert/Alert.tsx
+++ src/components/atoms/Alert/Alert.tsx
@@ -7,33 +7,33 @@ export const B4HAlert: React.FC<B4HAlertProps> = memo(({ type, text }) => {
   const textColor = ['text-yellow-500', 'text-red-500', 'text-green-500'];
   const bgColor = ['bg-yellow-500', 'bg-red-500', 'bg-green-500'];
 
-  return (
-    showAlert && (
-      <div
-        className="flex justify-center items-center self-center text-center bottom-7 fixed z-10 -left-0 w-full"
-        role="alert"
-      >
-        <div className="p-2">
-          <div
-            className={`inline-flex items-center bg-white leading-none rounded-full p-2 shadow text-teal text-lg ${textColor[type]}`}
+  return showAlert ? (
+    <div
+      className="flex justify-center items-center self-center text-center bottom-7 fixed z-10 -left-0 w-full"
+      role="alert"
+    >
+      <div className="p-2">
+        <div
+          className={`inline-flex items-center bg-white leading-none rounded-full p-2 shadow text-teal text-lg ${textColor[type]}`}
+        >
+          <span
+            className={`inline-flex text-white rounded-full h-6 px-3 justify-center items-center ${bgColor[type]}`}
           >
-            <span
-              className={`inline-flex text-white rounded-full h-6 px-3 justify-center items-center ${bgColor[type]}`}
-            >
-              {ETypes[type]}
-            </span>
-            <span className="inline-flex px-2">{text}</span>
-            <button
-              type="button"
-              onClick={() => {
-                setShowAlert(false);
-              }}
-            >
-              <span className="inline-flex px-2">×</span>
-            </button>
-          </div>
+            {ETypes[type]}
+          </span>
+          <span className="inline-flex px-2">{text}</span>
+          <button
+            type="button"
+            onClick={() => {
+              setShowAlert(false);
+            }}
+          >
+            <span className="inline-flex px-2">×</span>
+          </button>
         </div>
       </div>
-    )
+    </div>
+  ) : (
+    <> </>
   );
 });
