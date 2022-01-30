c3db62b8adf3242412701694f25e8179e53e6666
Paulo Bordignon <p.h_@outlook.com>
2022-01-30 00:08:53 -0300

    fix(components): fixed bug in sidebar render menu

---
+++ src/components/molecules/Sidebar/SidebarContent.tsx
@@ -0,0 +1,80 @@
+import React, { memo, useCallback, useState } from 'react';
+
+import { SubmenuProps, ContentProps } from './types';
+
+export const B4HSidebarContent: React.FC<ContentProps> = memo(
+  ({ currentLang, menu }) => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenu = useCallback(() => {
+      setMenuOpen(!menuOpen);
+    }, [menuOpen]);
+
+    const renderMenuItems = useCallback(() => {
+      return (
+        <div>
+          <button
+            className="flex items-center justify-between w-full px-6 py-2  focus:outline-none"
+            onClick={() => handleMenu()}
+          >
+            <span className="flex items-center">
+              <span className="mx-4 font-medium">
+                {currentLang === 'en'
+                  ? menu.title.en
+                  : currentLang === 'es'
+                  ? menu.title.es
+                  : menu.title.pt}
+              </span>
+            </span>
+            <span>
+              <svg
+                className={`w-4 h-4 transition-transform transform ${
+                  menuOpen ? 'rotate-180' : ''
+                }`}
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {menuOpen && (
+                  <path
+                    d="M19 9l-7 7-7-7"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                )}
+                {!menuOpen && (
+                  <path
+                    d="M19 9l-7 7-7-7"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                )}
+              </svg>
+            </span>
+          </button>
+          {menuOpen && (
+            <div>
+              {menu.routes.map((submenu: SubmenuProps) => {
+                return (
+                  <a
+                    key={submenu.path}
+                    className="block px-16 py-2 text-sm hover:bg-green-500 rounded-lg"
+                    href="#"
+                  >
+                    {submenu.title.en}
+                  </a>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      );
+    }, [handleMenu, menuOpen, currentLang, menu]);
+
+    return renderMenuItems();
+  }
+);
--- src/components/molecules/Sidebar/types.ts
+++ src/components/molecules/Sidebar/types.ts
@@ -1,10 +1,17 @@
-export type menu = {
+export type MenuProps = {
   title: { en: string; es: string; pt: string };
   heading: boolean;
   routes: any;
 };
 
-export type submenu = {
+export type SubmenuProps = {
   title: { en: string; es: string; pt: string };
   path: string;
+};
+
+export type ContentProps = {
+  menuOpen?: boolean;
+  currentLang: any;
+  handleMenu?: () => void;
+  menu?: any;
 };
